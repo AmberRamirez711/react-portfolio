4e77fdc147f3e50869f5a65d59ea6977e5d9cd2c
AmberRamirez711 <amberramirez@icloud.com>
2020-04-07 12:11:01 -0700

    Updated Portfolio

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
 import axios from 'axios';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import NavigationContainer from './navigation/navigation-container';
 import Home from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact';
 import Blog from './pages/blog';
+import BlogDetail from './pages/blog-detail';
 import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from './portfolio/portfolio-detail';
 import Auth from './pages/auth';
 import NoMatch from './pages/no-match';
+import Icons from "../helpers/icons";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+
+    Icons();
 
     this.state = {
       loggedInStatus: "NOT_LOGGED_IN"
@@ -44,27 +48,25 @@ export default class App extends Component {
   }
 
   checkLoginStatus() {
-    return axios.get("https://api.devcamp.space/logged_in", { 
+    return axios
+      .get("https://api.devcamp.space/logged_in", { 
       withCredentials: true 
-    }).then(response => {
-      const loggedIn = response.data.logged_in;
-      const loggedInStatus = this.state.loggedInStatus;
+      })
+      .then(response => {
+        const loggedIn = response.data.logged_in;
+        const loggedInStatus = this.state.loggedInStatus;
 
-      // if loggedIn and status is LOGGED_IN => return data
-      // if loggedIn status NOT_LOGGED_IN => update state
-      // if not loggedIn and status LOGGED_IN => update state
-
-      if(loggedIn && loggedInStatus === "LOGGED_IN") {
-        return loggedIn;
-      } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
-        this.setState({
+        if(loggedIn && loggedInStatus === "LOGGED_IN") {
+          return loggedIn;
+        } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+          this.setState({
           loggedInStatus: "LOGGED_IN"
         });
-      } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
-        this.setState({
-          loggedInStatus: "NOT_LOGGED_IN"
-        });
-      }
+        } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN"
+          });
+        }
     })
     .catch(error => {
       console.log("Error", error);
@@ -76,7 +78,13 @@ export default class App extends Component {
   }
 
   authorizedPages() {
-    return [<Route path="/portfolio-manager" component={PortfolioManager} />];
+    return [
+      <Route 
+        key="portfolio-manager"
+        path="/portfolio-manager" 
+        component={PortfolioManager} 
+      />
+    ];
   }
 
   render() {
@@ -105,7 +113,14 @@ export default class App extends Component {
               
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
-              <Route path="/blog" component={Blog} />
+
+              <Route path="/blog" 
+              render={props => (
+                <Blog {...props} loggedInStatus={this.state.loggedInStatus} />
+              )}
+               />
+
+              <Route path="/b/:slug" component={BlogDetail} />
               {this.state.loggedInStatus === "LOGGED_IN" ? (
                 this.authorizedPages()
               ) : null}
