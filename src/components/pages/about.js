c14a725d2cf9cc87b2bcaa744509ca4f3d0eb670
AmberRamirez711 <amberramirez@icloud.com>
2020-04-15 13:13:04 -0700

    Updated about me page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -14,23 +14,11 @@ export default  function() {
               />
 
               <div className="right-column">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quidem, in enim iure molestiae nam fugiat beatae! Iste recusandae eius animi vel vitae, praesentium aliquam consequatur 
-                cupiditate sapiente ex quasi! Provident, itaque. Quam eveniet maxime optio fugit mollitia ipsam perspiciatis quos? Nulla, corporis numquam quos consequuntur enim tempore est error,
-                voluptas voluptatem, ipsum quod. Animi nisi nemo aliquid fuga impedit? Officiis dignissimos possimus esse at aperiam modi unde neque magnam non dolorum, adipisci qui consectetur distinctio
-                reprehenderit rerum error nihil perspiciatis repudiandae? Numquam earum impedit facere, facilis accusamus nisi distinctio?
-                Atque itaque at, quidem voluptate magnam, quas numquam repellendus debitis, earum culpa sed. Minima commodi sapiente, omnis harum laboriosam a asperiores consequuntur provident officiis. 
-                Tempora perspiciatis dolores maxime architecto voluptatibus. Minima voluptas ipsa odio quisquam libero eius sint eos nobis labore molestiae, exercitationem voluptatum inventore laudantium 
-                consequuntur cupiditate amet impedit quia facere corrupti veniam autem, excepturi harum magni. Dolor, eaque! Iure velit exercitationem beatae molestiae maxime. Voluptates architecto deserunt
-                nulla aut sequi, fugiat ducimus quas veniam officiis, inventore harum qui deleniti ab odio, perspiciatis autem corrupti. Possimus illo eos consectetur. Ducimus, facere inventore officiis
-                dolorem impedit cupiditate vel beatae quod repudiandae aspernatur fugit dignissimos blanditiis itaque voluptatem aliquid temporibus repellat rem enim neque necessitatibus ipsa consequatur
-                vero quibusdam. Magnam, voluptates.
-                Quam doloribus sint aliquam, quo aut eaque quisquam hic id recusandae laborum commodi. Doloremque suscipit magnam, maiores recusandae voluptas neque esse excepturi fugit, nostrum cupiditate,
-                ea facere officia ducimus tempora! Error cupiditate incidunt facilis delectus nihil, magnam, natus, fugit atque assumenda repellendus eligendi nulla! Esse exercitationem magnam maiores,
-                enim ea, deserunt vel, minus blanditiis odio tempore molestiae optio reprehenderit itaque! Non odit commodi quasi nemo vitae tempore neque, tempora, magnam, ipsa perspiciatis quia voluptates.
-                Asperiores, quas! Assumenda ducimus ut eum quia enim laudantium recusandae eos velit soluta. Repudiandae, quas vitae! Deserunt, doloribus aliquid tempora commodi odio unde, eius dolor odit consequatur, 
-                quae iusto? Excepturi, debitis porro, laborum quod sint numquam minus necessitatibus pariatur animi facilis eum consequuntur perferendis amet soluta. Neque dicta, commodi ratione quod earum 
-                sit ipsum in dolores. At neque dolore eveniet necessitatibus provident, dolorem expedita molestiae vitae. Quis, dolorum
+              <h3>Born and raised in California (and now living in Las Vegas), I am a firm believer that you cannot succeed without hard work and a learning mentality.</h3>
 
+               <p>This shines in my work — whether it’s making sure I write code with its future maintainers in mind, or structuring my user-facing architecture with accessibility at the forefront.
+
+                I am highly adaptable and driven by learning. If there is a problem, I seek ways to fix it. Whether it takes reading a book or picking up a lesson in a framework I don’t know, I’ve grown to embrace the challenges that development can bring.</p>
             </div>
             </div>
         );
